d58c1c721e448c61b2928be0e7247782e95dad60
imthatalex <hernandezalex8765@gmail.com>
2022-11-03 22:16:29 -0400

    functional

--- src/App.js
+++ src/App.js
@@ -17,29 +17,31 @@ import {
   ModalCloseButton
 } from '@chakra-ui/react'
 
-// https://dpaste.org/HGeJr
+/* TO-DO */
 
-// created new product component : initial product component has unique state for button disabled, re renders when pushed to cart, resets state
-function ProductInCart({name, image, colors, quantity, products}){
+  // Style
 
-  const [updateProducts, setUpdateProducts] = useState(products);
+/* TO-DO */
 
 
+// Components Rendered in Cart
+function ProductInCart({name, image, colors}){
+
+  // State for Product Quantity
+  const [productQuantity, setProductQuantity] = useState(1);
+
+
+  // Using arrow expression to ensure the current value ~ S.R
+
+  // Issue #001 : Charkra Drawer Component : onClose : Resets Product Quantity State
   function incrementQuantity(){
-    setUpdateProducts(updateProducts.map(p => {
-      return {
-        ...p,
-        props: {
-          quantity: p.quantity + 1
-        }
-      }
-    }))
-    console.log(updateProducts);
+    setProductQuantity(productQuantity => productQuantity + 1);
   }
 
-
   function decrementQuantity(){
-    
+    if(productQuantity !== 0){
+      setProductQuantity(productQuantity => productQuantity - 1);
+    }
   }
 
   return(
@@ -52,20 +54,17 @@ function ProductInCart({name, image, colors, quantity, products}){
         {image}
       </div>
       <div className="row">
-        {quantity}
+        {productQuantity}
       </div>
       <div className="row">
         <ProductColors colors={colors}/>
       </div>
       <div className="row">
         <div className="col">
-          <ProductModal/>
+          <button className="product-bttn" onClick={incrementQuantity}>+1</button>
         </div>
         <div className="col">
-          <button onClick={incrementQuantity}>+1</button>
-        </div>
-        <div className="col">
-        <button onClick={decrementQuantity}>-1</button>
+        <button className="product-bttn" onClick={decrementQuantity}>-1</button>
         </div>
       </div>
     </div>
@@ -73,23 +72,65 @@ function ProductInCart({name, image, colors, quantity, products}){
   )
 }
 
+  // W3 Side Menu Component
+  function SideMenu({cart, pureProductDetails}){
 
-  // Cart Drawer 
-
-  function CartDrawer({cart, pureProductDetails}) {
-    // Controls
-    const { isOpen, onOpen, onClose } = useDisclosure()
-
-    // passes Product Components in Cart to an empty array
     const productsInCart = [];
 
-    // Limited : forLoop : if cart [0] (first entry) is equal to purePI[0] : (first entry = 'basketball')
-    function renderCart(pureProductDetails){
+    const [toggleMenu, setToggleMenu] = useState(false)
+
+    function openNav() {
+      setToggleMenu(true);
+    }
+
+    function closeNav() {
+      setToggleMenu(false);
+    }
+
+    pureProductDetails.forEach((item) => {
+      for(let i = 0; i < cart.length; i++){
+        if(cart[i] === item.props.name){
+          productsInCart.push(
+            <ProductInCart
+              key={item.props.id}
+              name={item.props.name}
+              image={item.props.image}
+              colors={item.props.colors}
+            />
+          );
+        }
+      }
+    })
+
+    return (
+      <>
+      <div id="mySidenav" style={{width: toggleMenu ? '250px' : '0'}} className="sidenav">
+        <button className="closebtn" onClick={closeNav}>Close</button>
+        {productsInCart}
+      </div>
+
+      <button className="product-bttn" onClick={openNav}>View Cart</button>
+      </>
+    )
+  }
+
+
+  // Chakra Drawer Component
+  function CartDrawer({cart, pureProductDetails}) {
+    // Chakra Controls
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+    const productsInCart = [];
+
+
+    // Note : Limitation : forLoop : if cart [0] (first entry) is equal to pureProductDetails[0] : (first entry = 'basketball') : does not check every value
 
     // using forEach removes the limitation forLoop had on iterating through the fixed length of pureProductDetails
     // now it simply tests the condition for all elements in the array regardless of length
 
-      pureProductDetails.map((item) => {
+    // checks which Products have been added to cart
+    // adds new Component to productsInCart reflecting the values of those in cart to Render those Products
+      pureProductDetails.forEach((item) => {
         for(let i = 0; i < cart.length; i++){
           if(cart[i] === item.props.name){
             productsInCart.push(
@@ -97,31 +138,24 @@ function ProductInCart({name, image, colors, quantity, products}){
                 key={item.props.id}
                 name={item.props.name}
                 image={item.props.image}
-                quantity={item.props.quantity}
                 colors={item.props.colors}
-                products={productsInCart}
               />
             );
           }
         }
       })
-    
-      // renders Components in Cart
-      return (
-        <div>{productsInCart}</div>
-      )
-    }
-  
+
     return (
       <>
         <Button onClick={onOpen}>View Cart</Button>
-        <Drawer placement='right' onClose={onClose} isOpen={isOpen}>
+        <Drawer placement='right' isOpen={isOpen}>
           <DrawerOverlay />
           <DrawerContent>
             <DrawerHeader borderBottomWidth='1px'>Basic Drawer</DrawerHeader>
             <DrawerBody>
-              {renderCart(pureProductDetails)}
+              {productsInCart}
             </DrawerBody>
+            <Button onClick={onClose}> Close Cart </Button>
           </DrawerContent>
         </Drawer>
       </>
@@ -130,7 +164,6 @@ function ProductInCart({name, image, colors, quantity, products}){
 
 
   // Navbar
-
 function Navbar({cart, pureProductDetails, cartQuantity}){
   return (
     <div className="container">
@@ -139,7 +172,7 @@ function Navbar({cart, pureProductDetails, cartQuantity}){
           <div>Home</div>
         </div>
         <div className="col">
-          <div></div>
+          <SideMenu cart={cart} pureProductDetails={pureProductDetails}/>
         </div>
         <div className="col">
           <div></div>
@@ -156,8 +189,7 @@ function Navbar({cart, pureProductDetails, cartQuantity}){
 }
 
 
-// Colors : Modal : Details : Add to Cart : Remove from Cart : Increment/Decrement Quantity
-
+// Reads colors property value from dataSet and creates a div element to render the colors
 function ProductColors({colors}){
   let exampleColors = [];
 
@@ -173,28 +205,33 @@ function ProductColors({colors}){
 }
 
 
- // Modal Example
-
- function ProductModal() {
+ // Chakra Modal Component
+function ProductModal({name, image, colors}) {
   const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <>
-      <Button onClick={onOpen}>View Product</Button>
+      <Button onClick={onOpen}>View</Button>
 
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader>{name}</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            hello
+          <div className="col">
+            <div className="row">
+              {image}
+            </div>
+            <div className="row">
+              <ProductColors colors={colors}/>
+            </div>
+          </div>
           </ModalBody>
 
           <ModalFooter>
             <Button colorScheme='blue' mr={3} onClick={onClose}>
               Close
             </Button>
-            <Button variant='ghost'>Secondary Action</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
@@ -202,17 +239,16 @@ function ProductColors({colors}){
   )
 }
 
-
+// Initial Product Component : Product Rendered in Home Page
 function ProductDetail({image, name, colors, cart, setCart, cartQuantity, setCartQuantity}){
 
+  // state used to disable add to cart func once product is added to cart to eliminate duplications
   const [buttonDisabled, setButtonDisabled] = useState(false); 
 
   function addToCart(){
-    // adds name of product to cart
     cart.push(name);
     setCart(cart);
-    // increments cart quantity
-    setCartQuantity(cartQuantity + 1);
+    setCartQuantity(cartQuantity => cartQuantity + 1);
     
     for(let i = 0; i < cart.length; i++){
       if(cart[i] === name) {
@@ -222,15 +258,14 @@ function ProductDetail({image, name, colors, cart, setCart, cartQuantity, setCar
   }
 
   function removeFromCart(){
-      // Limited : pop method : removes only last element from array
-      
+      // Note : Limitation : pop method : removes only last element from array
       // filter is iterating through cart : if element value is not equal to name value, return value, since the value is equal it does not return 
       setCart(cart.filter(element => element !== name));
-          
-      // ensures cart quantity does not drop into negative values : decrements cart quantity
+        
       if(cartQuantity !== 0){
         setCartQuantity(cartQuantity - 1);
       }
+      // reactivates add to cart button once product is removed from cart
       setButtonDisabled(false);
   }
 
@@ -254,7 +289,11 @@ function ProductDetail({image, name, colors, cart, setCart, cartQuantity, setCar
             <button className="product-bttn" onClick={removeFromCart}>Remove from Cart</button>  
           </div>
           <div className="col">
-            <ProductModal/>
+            <ProductModal
+              name={name}
+              image={image}
+              colors={colors}
+            />
           </div>
         </div>
       </div>
@@ -272,27 +311,16 @@ function ProductDetails({products}) {
   // displays number of products in cart
   const [cartQuantity, setCartQuantity] = useState(0);
 
-  
-  
-
-  // set state for product quantity and implement increment/decrement handler functions
-
-  // copy of products : cannot mutate original arrays : pure programming
+  // Note : cannot mutate original arrays : pure programming
   const pureProductDetails = [];
-
-  const [pureProducts, setPureProducts] = useState(pureProductDetails);
 
   products.forEach((product) => {
       pureProductDetails.push(
         <ProductDetail 
           key={product.id} 
-          id={product.id}
           image={product.image} 
           name={product.name} 
           colors={product.colors} 
-          quantity={product.quantity}
-          products={pureProducts}
-          setProducts={setPureProducts}
           cart={cart} 
           setCart={setCart} 
           cartQuantity={cartQuantity} 
@@ -311,8 +339,7 @@ function ProductDetails({products}) {
 }
 
 
-
-  export default function ProductGallery({products}){
+  export default function ProductList({products}){
     return(
       <div className="container">
         <div className="row">
--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import ProductGallery from './App';
+import ProductList from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react'
 
-
-
-// add quantity
 const productList = [
-  {id: 0, name: 'Basketball', colors: ['orange', 'blue', 'black'], image: 'Basketball Image', quantity: 3},
-  {id: 1, name: 'Football', colors: ['orange', 'blue', 'black'], image: 'Football Image', quantity: 9},
-  {id: 2, name: 'Baseball', colors: ['orange', 'blue', 'black'], image: 'Baseball Image', quantity: 7},
+  {id: 0, name: 'Basketball', colors: ['orange', 'blue', 'black'], image: 'Basketball Image'},
+  {id: 1, name: 'Football', colors: ['orange', 'blue', 'black'], image: 'Football Image'},
+  {id: 2, name: 'Baseball', colors: ['orange', 'blue', 'black'], image: 'Baseball Image'},
 ]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ChakraProvider>
     <React.StrictMode>
-      <ProductGallery products={productList} />
+      <ProductList products={productList} />
     </React.StrictMode>
   </ChakraProvider>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
